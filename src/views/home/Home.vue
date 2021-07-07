aae46a19431e7b15d2c741096fbd17d7363e0105
wwz0054 <1046676201@qq.com>
2021-07-07 15:27:58 +0800

    初始化项目

---
+++ src/views/home/Home.vue
@@ -0,0 +1,118 @@
+<template>
+  <div id="home">
+    <navbar class="home-nav"><div slot="center">购物街</div></navbar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control :title="['流行', '新款', '精选']" @tabClick="tabClick" />
+    <goods-list :goods="goods[currentType].list" />
+  </div>
+</template>
+ 
+<script>
+import HomeSwiper from "./childComps/HomeSwiper.vue";
+import RecommendView from "./childComps/RecommendView.vue";
+import Featureview from "./childComps/FeatureView.vue";
+
+import Navbar from "components/common/navbar/NavBar.vue";
+import TabControl from "components/content/tabControl/TabControl.vue";
+import GoodsList from "components/content/goods/GoodsList.vue";
+
+import { getHomeMultidata, getHomeGoods } from "network/home.js";
+
+export default {
+  name: "Home",
+  components: {
+    HomeSwiper,
+    RecommendView,
+    Featureview,
+    Navbar,
+    TabControl,
+    GoodsList,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      currentType: "pop",
+    };
+  },
+  created() {
+    //1.请求多个数据
+    this.getHomeMultidata();
+    //请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    //事件监听相关方法
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+
+    //网络请求相关方法
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, 1).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#home {
+  position: relative;
+}
+
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0px;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
+
+.content {
+  overflow: hidden;
+
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+</style>
